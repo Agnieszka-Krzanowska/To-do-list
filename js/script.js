7ce17da6f360e44b2635a621fbb8f4a7c0a88dcb
Agnieszka Krzanowska <agnieszka.krzanowska1998@gmail.com>
2022-11-18 13:33:11 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+{
+    const tasks = [
+        {
+            content:"",
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+             ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
