b43cca55cdd0cc40e2fa2b90748ae9e90dc37444
Agnieszka Krzanowska <agnieszka.krzanowska1998@gmail.com>
2022-11-21 13:26:12 +0100

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 {
     const tasks = [
         {
-            content:"",
+            content: "",
         },
     ];
 
@@ -19,8 +19,31 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+    
+    const onFormSubmit = (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+
+            addNewTask(newTaskContent);
+        };
+
     const init = () => {
         render();
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
